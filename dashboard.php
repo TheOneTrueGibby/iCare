0ba4a7357bbb9e493216457afe25695c79080c18
dmmiller155 <dami3857@vandals.uidaho.edu>
2024-03-03 11:12:56 -0800

    added dashboard and reg pages
    (-_-)

--- dashboard.php
+++ dashboard.php
@@ -9,10 +9,68 @@ include("session.php");
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-	<link rel="stylesheet" href="style.css">
+	<link href="style.css" rel="stylesheet">
 	</head>
 <body>
-	Welcome to my bad ass webste 
-	<p><a href="index.php">Logout</a></p>
+	
+  <div class="container dashboard-container">
+    <h1>Dashboard</h1>
+  
+    <br>
+    <br>
+    <br>
+
+    <div class="row">
+        <div class="col-md-4">
+        <a href="insurance.php" class="card btn btn-primary">
+            <div class="card-body">
+                <h5 class="card-title">Mortgages and insurances</h5>
+                <p class="card-text">Click here to perform action 1.</p>
+            </div>
+        </a>
+    </div>
+    <div class="col-md-4">
+      <a href="Lawn.php" class="card btn btn-primary">
+        <div class="card-body">
+          <h5 class="card-title">Lawn care</h5>
+          <p class="card-text">Click here to perform action 2.</p>
+        </div>
+      </a>
+    </div>
+    <div class="col-md-4">
+      <a href="Interior.php" class="card btn btn-primary">
+        <div class="card-body">
+          <h5 class="card-title">Interior care</h5>
+          <p class="card-text">Click here to perform action 3.</p>
+        </div>
+      </a>
+    </div>
+    <div class="col-md-4">
+      <a href="Internet.php" class="card btn btn-primary">
+        <div class="card-body">
+          <h5 class="card-title">Internet sevices</h5>
+          <p class="card-text">Click here to perform action 4.</p>
+        </div>
+      </a>
+    </div>
+    <div class="col-md-4">
+      <a href="Phone.php" class="card btn btn-primary">
+        <div class="card-body">
+          <h5 class="card-title">Phone Services</h5>
+          <p class="card-text">Click here to perform action 5.</p>
+        </div>
+      </a>
+    </div>
+    <div class="col-md-4">
+      <a href="index.php" class="card btn btn-primary">
+        <div class="card-body">
+          <h5 class="card-title">Logout</h5>
+        </div>
+      </a>
+    </div>
+  </div>
+
+</div>
+
 </body>
 </html>
---
+++ lawn.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Registration</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<?php
+    require('db.php');
+    if (isset($_REQUEST['username'])) {
+        $username = stripslashes($_REQUEST['username']);
+        $username = mysqli_real_escape_string($con, $username);
+        $email    = stripslashes($_REQUEST['email']);
+        $email    = mysqli_real_escape_string($con, $email);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        $fname    = stripslashes($_REQUEST['fname']);
+        $fname    = mysqli_real_escape_string($con, $fname);
+        $lname    = stripslashes($_REQUEST['lname']);
+        $lname    = mysqli_real_escape_string($con, $lname);
+        $age    = stripslashes($_REQUEST['age']);
+        $age    = mysqli_real_escape_string($con, $age);
+        $income    = stripslashes($_REQUEST['income']);
+        $income    = mysqli_real_escape_string($con, $income);
+        $hasphone    = stripslashes($_REQUEST['hasphone']);
+        $hasphone    = mysqli_real_escape_string($con, $hasphone);
+        $create_datetime = date("Y-m-d H:i:s");
+        $query    = "INSERT into `residents` (username, password, email, fname, lname, age, income, hasphone, create_datetime)
+                     VALUES ('$username', '" . md5($password) . "', '$email', '$fname', '$lname', '$age', '$income', '$hasphone', '$create_datetime')";
+        $result   = mysqli_query($con, $query);
+        if ($result) {
+            echo "<div class='form'>
+                  <h3>You have registered a new user successfully.</h3><br/>
+                  <p class='link'>Click here to <a href='index.php'>Login</a></p>
+                  </div>";
+        } else {
+            echo "<div class='form'>
+                  <h3>Required fields are missing.</h3><br/>
+                  <p class='link'>Click here to <a href='dashboard.php'>register</a> again.</p>
+                  </div>";
+        }
+    } else { 
+?>
+    <form class="form" action="" method="post">
+        <h1 class="login-title">Registration</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" required />
+        <input type="password" class="login-input" name="password" placeholder="Password">
+        <input type="text" class="login-input" name="email" placeholder="Email Adress">
+        <input type="text" class="login-input" name="fname" placeholder="First Name">
+        <input type="text" class="login-input" name="lname" placeholder="Last Name">
+        <input type="number" class="login-input" name="age" placeholder="Your Age">
+        <input type="number" class="login-input" name="income" placeholder="Average Income">
+        <p> Do you have a phone? <input type="checkbox" class="login-input" name="hasphone" placeholder="Do You Have A Phone?"> </p>
+        <input type="submit" name="submit" value="Register" class="login-button">
+        <p class="link"><a href="index.php">Click to Login</a></p>
+    </form>
+<?php
+    }
+?>
+</body>
+</html>
